19e62509f41f0abd96b0db53c15ea92bed18108d
Shresthsharma414 <145872853+Shresthsharma414@users.noreply.github.com>
2025-01-01 19:39:07 +0530

    Update LoginRepository.java

--- src/main/java/com/LSS/repository/LoginRepository.java
+++ src/main/java/com/LSS/repository/LoginRepository.java
@@ -1,25 +1,13 @@
 package com.LSS.repository;
-<<<<<<< HEAD
 import java.util.List;
-import java.util.Optional;
-=======
->>>>>>> dbfb524 (added accommodator model)
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.LSS.models.Login;
-<<<<<<< HEAD
-@Repository
-public interface LoginRepository extends JpaRepository<Login,Long> {
-	
-
-	public List<Login> findPassByUsernameAndPass(String username,String password);
-	
-=======
-
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Long> {
->>>>>>> dbfb524 (added accommodator model)
-
+    List<Login> findByLoginIdAndPassword(String loginId, String password);
 }
+
