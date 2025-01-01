c5dcb4bbe2d4f4150787d2b6188594b52195478d
Shresthsharma414 <145872853+Shresthsharma414@users.noreply.github.com>
2025-01-01 18:57:06 +0530

    added accommodator

--- src/main/java/com/LSS/models/Login.java
+++ src/main/java/com/LSS/models/Login.java
@@ -1,7 +1,6 @@
 package com.LSS.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,52 +10,60 @@ import jakarta.persistence.Table;
 @Entity
 @Table
 public class Login {
-private String LoginId;
-private String Password;
-private String Authentication;
-@Id
-@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Login_Sequence")
-@SequenceGenerator(name = "Login_Sequence", sequenceName = "Login_Sequence", allocationSize = 1)
-private Long id ;
-	public Login(String loginId, String password, String authentication) {
-	super();
-	LoginId = loginId;
-	Password = password;
-	Authentication = authentication;
-	
-}
-	@Override
-	public String toString() {
-		return "Login [LoginId=" + LoginId + ", Password=" + Password + ", Authentication=" + Authentication + ", id="
-				+ id + "]";
-	}
-	public Login() {
-		
-	}
-	public String getLoginId() {
-		return LoginId;
-	}
-	public void setLoginId(String loginId) {
-		LoginId = loginId;
-	}
-	public String getPassword() {
-		return Password;
-	}
-	public void setPassword(String password) {
-		Password = password;
-	}
-	public String getAuthentication() {
-		return Authentication;
-	}
-	public void setAuthentication(String authentication) {
-		Authentication = authentication;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
 
+    private String loginId;
+    private String password;
+    private Long authenticationId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Login_Sequence")
+    @SequenceGenerator(name = "Login_Sequence", sequenceName = "Login_Sequence", allocationSize = 1)
+    private Long id;
+
+    public Login(String loginId, String password, Long authenticationId) {
+        super();
+        this.loginId = loginId;
+        this.password = password;
+        this.authenticationId = authenticationId;
+    }
+
+    public Login() {}
+
+    @Override
+    public String toString() {
+        return "Login [loginId=" + loginId + ", password=" + password + ", authenticationId=" + authenticationId
+                + ", id=" + id + "]";
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getAuthenticationId() {
+        return authenticationId;
+    }
+
+    public void setAuthenticationId(Long authenticationId) {
+        this.authenticationId = authenticationId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
